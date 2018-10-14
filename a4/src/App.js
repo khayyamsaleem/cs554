885eb069fa031cc4c4d74df407ee945ab6299a6c
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-10-14 11:48:11 -0400

    started assignment 4, switching to backtop bc macbook keyboard is fkd

---
+++ a4/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles/app.scss"
+import MDEditor from "./views/MDeditor"
+
+export const App = ()  => {
+    <MDEditor />
+}
---
+++ a4/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MonacoWebpackPlugin()
+  ]
+};
